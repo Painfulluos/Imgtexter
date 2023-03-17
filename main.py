11a330c2937305270fd0a5eff42829b2b12b7660
Painfulluos <painfulluos@gmail.com>
2023-03-17 15:08:47 +0700

    Update main.py
    ~add context menu(File->Exit,Settings->Languages)

--- main.py
+++ main.py
@@ -7,6 +7,20 @@ root.geometry("400x400")
 root.resizable(False,False)
 
 
+main_menu = tk.Menu(root)
+root.config(menu=main_menu)
+
+file_menu = tk.Menu(main_menu, tearoff=0)
+file_menu.add_command(label="Exit", command=exit)
+
+settings_menu = tk.Menu(main_menu, tearoff=0)
+settings_menu.add_command(label="Language")
+
+
+main_menu.add_cascade(label="File", menu=file_menu)
+main_menu.add_cascade(label="Settings", menu=settings_menu)
+
+
 label_btn_convert = tk.Label(root, text="Choose image:")
 btn_convert = tk.Button(root, text="Open", command=lambda:logic.imgToString(root))
 
